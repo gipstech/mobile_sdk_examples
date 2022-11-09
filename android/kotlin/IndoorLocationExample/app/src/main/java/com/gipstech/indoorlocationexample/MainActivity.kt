f1eb9223e0302fbbb9a80e8f61e142c20cddaef6
gipstech <info@gipstech.com>
2022-11-09 14:36:50 +0100

    Add kotlin app IndoorLocationExample

---
+++ android/kotlin/IndoorLocationExample/app/src/main/java/com/gipstech/indoorlocationexample/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.gipstech.indoorlocationexample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+import com.gipstech.*
+
+class MainActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
+
+    val listener = object: IndoorLocalizer.Listener {
+        override fun onRegistrationComplete() {
+            textView?.append("Registration completed\n")
+        }
+
+        override fun onRegistrationUpdate(progress: Int) {
+            textView?.append("Registration progress: " + progress + "%\n")
+        }
+
+        override fun onLocationUpdate(location: IndoorLocation) {
+            textView?.append("Location: " + location.longitude + " " + location.latitude + "\n")
+        }
+
+        override fun onFloorChange(floor: Floor): Boolean {
+            textView?.append("Floor: " + floor.name + "\n")
+            return true
+        }
+
+        override fun onAttitudeUpdate(attitude: Attitude) {
+            textView?.append("Heading: " + attitude.heading + "\n")
+        }
+
+        override fun onError(error: GiPStechError) {
+            textView?.append(error.message + "\n")
+
+            if (GiPStechError.FAST_CALIBRATION_REQUIRED == error.code) {
+                performFastCalibration()
+            }
+        }
+    }
+
+    fun performFastCalibration() {
+        GiPStech.getCalibrationManager().beginCalibration(CalibrationManager.CALIBRATION_TYPE_FAST, object: ProgressCallback<Void?> {
+            override fun onResult(result: Void?) {
+                textView?.append("Calibration completed\n")
+                GiPStech.getIndoorLocalizer().registerListener(listener)
+            }
+
+            override fun onProgress(progress: Int) {
+                textView?.append("Calibration progress: " + progress + "%\n")
+
+                if (progress >= 25) {
+                    GiPStech.getCalibrationManager().endCalibration()
+                }
+            }
+
+            override fun onError(error: GiPStechError) {
+                textView?.append(error.message + "\n")
+            }
+        })
+    }
+
+    fun allGranted(grantResults: IntArray): Boolean {
+        if (grantResults.isEmpty()) {
+            return false
+        }
+        for (res in grantResults) {
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun startLocation() {
+        GiPStech.getIndoorLocalizer().selectFloor("id of the floor", object: ProgressCallback<Void?> {
+            override fun onResult(result: Void?) {
+                textView?.append("Selection completed\n")
+                GiPStech.getIndoorLocalizer().registerListener(listener)
+            }
+
+            override fun onProgress(progress: Int) {
+                textView?.append("Selection progress: " + progress + "%\n")
+            }
+
+            override fun onError(error: GiPStechError) {
+                textView?.append(error.message + "\n")
+            }
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.mainTextView)
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.BLUETOOTH
+        )
+
+        GiPStech.init(this, "insert the development key associated to your app")
+
+        if (
+            checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(permissions, 0)
+        } else {
+            startLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!allGranted(grantResults)) {
+            textView?.append("Please grant all permissions to start location")
+            return
+        }
+
+        startLocation()
+    }
+}
